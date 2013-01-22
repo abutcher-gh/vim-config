af63196911bf8127fc0fdeb9bd6d637541dd31a5
Adam Butcher <adam@jessamine.co.uk>
2013-01-22 21:15:20 +0000

    quick-tags-cscope: Support FORCE_DIR option within git tree.

--- bin/quick-tags-cscope.sh
+++ bin/quick-tags-cscope.sh
@@ -29,17 +29,21 @@ else
    CTAGS_LINK_OPTS=(--links=no)
    FIND_LINK_OPTS=(-not -type l)
 fi
-if [ -n "$FORCE_DIR" ]; then
-   cd "$1"
-   rm -f cscope* tags
-elif [ -z "$NO_GIT" ]; then
+if [ -z "$NO_GIT" ]; then
    if [[ "$OS" = *Windows* ]]; then NIL=NUL; else NIL=/dev/null; fi
    if REPO_ROOT=$(cd "$1" && git rev-parse --show-toplevel 2>$NIL); then
-      cd "$REPO_ROOT"
-      rm -f cscope* tags
-      echo >&2 "Locating files in '$REPO_ROOT' with 'git'..."
-      git ls-files '*.c??' '*.c' '*.h' '*.h??' '*.inl' '*.impl' '*.java' | sed "s|^|$PWD/|" > cscope.files
+      rm -f "$REPO_ROOT/"cscope* "$REPO_ROOT/"tags
+      if [ -n "$FORCE_DIR" ]
+      then SRC_DIR="$(cd "$1" && pwd)"
+      else SRC_DIR="$REPO_ROOT"
+      fi
+      echo >&2 "Locating files in '$SRC_DIR' with 'git'..."
+      (cd "$SRC_DIR" && git ls-files --full-name '*.c??' '*.c' '*.h' '*.h??' '*.inl' '*.impl' '*.java') | sed "s|^|$REPO_ROOT/|" > "$REPO_ROOT"/cscope.files
+      cd $REPO_ROOT
    fi
+elif [ -n "$FORCE_DIR" ]; then
+   cd "$1"
+   rm -f cscope* tags
 fi
 [ -n "$REPO_ROOT" ] || { echo >&2 "Locating files in '$1' with 'find'..."; find . -regex '.*\.\([chi]\(pp\|xx\|\+\+\)?\|inl\|impl\|java\)' ${FIND_LINK_OPTS[@]} > cscope.files; }
 echo >&2 "Updating tags..."
